276554b946ac2642c31f98e26842ea2dfda1ade5
Bin289 <82301249+NTN014@users.noreply.github.com>
2023-10-28 22:28:42 +0700

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.meme"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.meme"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
