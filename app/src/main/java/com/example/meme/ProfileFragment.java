1de5ae85663917c6e44c247388af4f3afdcf1713
Ky Anh <trankyanh202@gmail.com>
2023-11-12 16:22:30 +0700

    Profile login and register layout

--- app/src/main/java/com/example/meme/ProfileFragment.java
+++ app/src/main/java/com/example/meme/ProfileFragment.java
@@ -1,12 +1,19 @@
 package com.example.meme;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+
 
 public class ProfileFragment extends Fragment {
 
@@ -14,6 +21,26 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        Button logginButton = view.findViewById(R.id.loginButton);
+
+        logginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                openLoginScreen();
+            }
+        });
+        return view;
     }
+
+    private void openLoginScreen() {
+        FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.container, new LoginFragment());
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
 }
