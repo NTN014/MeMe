7f7a567c790b0d21c88fb136b51f926667e1f5d0
Ky Anh <trankyanh202@gmail.com>
2023-11-17 23:31:20 +0700

    status of the user in profile

--- app/src/main/java/com/example/meme/LoginActivity.java
+++ app/src/main/java/com/example/meme/LoginActivity.java
@@ -1,42 +1,134 @@
 package com.example.meme;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
+import android.app.AlertDialog;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
 
 
 public class LoginActivity extends AppCompatActivity {
+    private DatePickerDialog datePickerDialog;
+    private Button datebutton;
+    private EditText Email, Name, phone;
+    public static final String EXTRA_EMAIL = "extra_email";
+    public static final String EXTRA_USERNAME = "extra_username";
+    public static final String EXTRA_PHONE_NUMBER = "extra_phone_number";
+    public static final String EXTRA_DATE_OF_BIRTH = "extra_date_of_birth";
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        TextView registerTextView = findViewById(R.id.registerBut);
-        registerTextView.setOnClickListener(new View.OnClickListener() {
+        initDatePicker();
+        datebutton = findViewById(R.id.ageselect);
+        datebutton.setText(getTodaysDate());
+        Email = findViewById(R.id.email);
+        Name = findViewById(R.id.name);
+        phone = findViewById(R.id.phonenumber);
+        Button signin = findViewById(R.id.login);
+        signin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openRegisterActivity();
+                String email = Email.getText().toString();
+                String phoneNumber = phone.getText().toString();
+                String name = Name.getText().toString();
+                String dateOfBirth = datebutton.getText().toString();
+                sendUsernameToProfile(email, name, phoneNumber, dateOfBirth);
             }
         });
     }
 
 
-    private void openRegisterActivity() {
-        Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-        startActivity(intent);
+    private void sendUsernameToProfile(String email, String name, String phoneNumber, String dateOfBirth) {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        intent.putExtra(EXTRA_USERNAME, name);
+        intent.putExtra(EXTRA_PHONE_NUMBER, phoneNumber);
+        intent.putExtra(EXTRA_EMAIL, email);
+        intent.putExtra(EXTRA_DATE_OF_BIRTH, dateOfBirth);
+        setResult(RESULT_OK, intent);
+        finish();
     }
+
+    private String getTodaysDate() {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        month = month + 1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        return makeDateString(day, month, year);
+    }
+
+    private String makeDateString(int day, int month, int year) {
+        return getMonthFormat(month) + " " + day + " " + year;
+    }
+
+    private String getMonthFormat(int month) {
+        if(month == 1)
+            return "JAN";
+        if(month == 2)
+            return "FEB";
+        if(month == 3)
+            return "MAR";
+        if(month == 4)
+            return "APR";
+        if(month == 5)
+            return "MAY";
+        if(month == 6)
+            return "JUN";
+        if(month == 7)
+            return "JUL";
+        if(month == 8)
+            return "AUG";
+        if(month == 9)
+            return "SEP";
+        if(month == 10)
+            return "OCT";
+        if(month == 11)
+            return "NOV";
+        if(month == 12)
+            return "DEC";
+
+
+        return "JAN";
+    }
+
+    private void initDatePicker() {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener()
+        {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day)
+            {
+                month = month + 1;
+                String date = makeDateString(day, month, year);
+                datebutton.setText(date);
+            }
+        };
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+
+        datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
+
+    }
+
+    public void OnDatePicker(View view){
+
+        datePickerDialog.show();
+    }
+
 }
--- app/src/main/java/com/example/meme/ProfileFragment.java
+++ app/src/main/java/com/example/meme/ProfileFragment.java
@@ -4,18 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.Navigation;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 
 public class ProfileFragment extends Fragment {
+
+    private static final int LOGIN_REQUEST_CODE = 123 ;
+    private TextView email, phone, Uname, dob;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -24,6 +27,10 @@ public class ProfileFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
         Button logginButton = view.findViewById(R.id.loginButton);
+        email = view.findViewById(R.id.email);
+        phone = view.findViewById(R.id.phoneNumberTextView);
+        Uname = view.findViewById(R.id.name);
+        dob = view.findViewById(R.id.dateofbirth);
 
         logginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,9 +42,50 @@ public class ProfileFragment extends Fragment {
         return view;
     }
 
+
+
     private void openLoginScreen() {
         Intent intent = new Intent(requireContext(), LoginActivity.class);
-        startActivity(intent);
+        startActivityForResult(intent, LOGIN_REQUEST_CODE);
+    }
+
+
+
+    //for userdata
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == LOGIN_REQUEST_CODE && resultCode == LoginActivity.RESULT_OK && data != null) {
+            String emailValue = data.getStringExtra(LoginActivity.EXTRA_EMAIL);
+            String name = data.getStringExtra(LoginActivity.EXTRA_USERNAME);
+            String phoneNumber = data.getStringExtra(LoginActivity.EXTRA_PHONE_NUMBER);
+            String dateOfBirth = data.getStringExtra(LoginActivity.EXTRA_DATE_OF_BIRTH);
+
+            displayUsername(name);
+            phone.setText(phoneNumber);
+            email.setText(emailValue);
+            displayDateOfBirth(dateOfBirth);
+
+            TextView emailLabel = getView().findViewById(R.id.emailLabel);
+            TextView nameLabel = getView().findViewById(R.id.nameLabel);
+            TextView phoneLabel = getView().findViewById(R.id.phoneLabel);
+            TextView dobLabel = getView().findViewById(R.id.doblable);
+            emailLabel.setText("Email:  ");
+            nameLabel.setText("Name:  ");
+            phoneLabel.setText("Phone:  ");
+            dobLabel.setText("Birth year:  ");
+            if (!TextUtils.isEmpty(name) && !TextUtils.isEmpty(phoneNumber) && !TextUtils.isEmpty(emailValue)) {
+                Button logginButton = requireView().findViewById(R.id.loginButton);
+                logginButton.setVisibility(View.GONE); // Hide the login button
+            }
+        }
+    }
+
+    private void displayUsername(String name) {
+        Uname.setText(name);
+    }
+    private void displayDateOfBirth(String dateOfBirth) {
+        dob.setText(dateOfBirth);
     }
 
 }
