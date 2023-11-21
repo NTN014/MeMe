87e07dbb4c95c0e62e383355c31544220ecb4bf3
KzGM <honghia12344@gmail.com>
2023-11-21 11:30:24 +0700

    Add Darkmode to setting (Not working yet)

--- app/src/main/java/com/example/meme/activity/SettingFragment.java
+++ app/src/main/java/com/example/meme/activity/SettingFragment.java
@@ -3,23 +3,27 @@ package com.example.meme.activity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CompoundButton;
 
 import com.example.meme.R;
 
 public class SettingFragment extends Fragment {
 
     Button btnMoveToAddFood, btnMoveToAddProduct;
+    SwitchCompat modeSwitch;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_setting, container, false);
-
+        modeSwitch = view.findViewById(R.id.modeSwitch);
         btnMoveToAddFood = view.findViewById(R.id.btnMoveToAddFood);
         btnMoveToAddProduct = view.findViewById(R.id.btnMoveToAddProduct);
 
@@ -28,6 +32,17 @@ public class SettingFragment extends Fragment {
             public void onClick(View v) {
                 Intent intent = new Intent(getContext(), AddFoodActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        modeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                }
             }
         });
 
